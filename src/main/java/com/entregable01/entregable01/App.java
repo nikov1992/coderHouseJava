05e5905aa37dd4dc1846cbbb6f82f7be00e40fdc
NICOLAS <nicolas.lopez1919@gmail.com>
2024-02-24 23:27:51 -0300

    primer commit

---
+++ src/main/java/com/entregable01/entregable01/App.java
@@ -0,0 +1,13 @@
+package com.entregable01.entregable01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
