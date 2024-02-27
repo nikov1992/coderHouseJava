e50e809c97c66825c55fa0ff7a8f5fdfb9e41955
NICOLAS <nicolas.lopez1919@gmail.com>
2024-02-27 20:50:32 -0300

    CLASE 3 DE JAVA

---
+++ src/main/java/clase02/Practice.java
@@ -0,0 +1,14 @@
+package clase02;
+
+public class Practice {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola mundo!!");
+		System.out.println("probando los commit");
+
+	}
+
+}
---
+++ src/main/java/clase03/Listas.java
@@ -0,0 +1,41 @@
+package clase03;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+public class Listas {
+
+    public static void main(String[] args) {
+
+
+    List<String>colores = new ArrayList<>();
+
+        colores.add("Blanco");
+        colores.add("Rojo");
+        colores.add("Negro");
+
+        Iterator<String> iterator = colores.iterator();
+
+        for(String color: colores){
+            System.out.println("los colores son: " + color);
+        }
+
+        System.out.println("El tanio de colores es: " + colores.size());
+
+       /* while(iterator.hasNext()){
+            String color = iterator.next();
+            System.out.println("Color" + color);
+        }*/
+
+        do{
+            System.out.println("ESTOY EN EL DO");
+            String color = iterator.next();
+            System.out.println("Color" + color);
+        }while(iterator.hasNext());
+
+
+    }
+
+
+
+}
